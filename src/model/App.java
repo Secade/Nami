73a1d822c526421632758c8d12a4dab9e3c5c5f9
MylesRC <myles_chan@dlsu.edu.ph>
2019-06-14 23:54:02 +0800

    added register and login to App

--- src/model/App.java
+++ src/model/App.java
@@ -1,14 +1,37 @@
 package model;
 
+import javafx.collections.ObservableList;
+
+import java.sql.SQLException;
+
 public class App {
     private Account account;
+    private AccountService accountService = new AccountService();
 
     public App() {
         account = null;
     }
 
+    private  void register(String username, String password, String email, String phone) throws SQLException {
+        Account account = new Account(username, password, email, phone);
+        accountService.add(account);
+    }
+
     public void login(Account account) {
         this.account = account;
+    }
+
+    public boolean login(String email, String password) throws SQLException {
+        ObservableList<Object> emails = accountService.getAll();
+        for(Object o : emails){
+            if(((Account)o).getEmail().equals(email)) {
+                if (((Account)o).getPassword().equals(password)) {
+                    this.account = ((Account)o);
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public void logout() {
